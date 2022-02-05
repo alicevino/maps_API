ca85a3cad5ce2e1c62616bdb2be582d396445247
Alice Vinigradova <zazaliska@gmail.com>
2022-02-05 15:42:24 +0300

    task 2 done

--- задача 2.py
+++ задача 2.py
@@ -9,6 +9,8 @@ class Params(object):
     def __init__(self):
         self.latitude = 55.753630
         self.longitude = 37.620070
+        self.spn = 0.003125
+        self.type = 'map'
 
     def ll(self):
         answer = str(self.longitude) + ',' + str(self.latitude)
@@ -16,7 +18,7 @@ class Params(object):
 
 
 def load_map(map):
-    map_request = "http://static-maps.yandex.ru/1.x/?ll={ll}&spn=0.04,0.04&l=map".format(ll=map.ll())
+    map_request = f"http://static-maps.yandex.ru/1.x/?ll={map.ll()}&spn={map.spn},{map.spn}&l=map"
     response = requests.get(map_request)
     if not response:
         print('Ошибка:', map_request)
@@ -39,22 +41,19 @@ my_map = Params()
 manager = pygame_gui.UIManager((600, 450))
 
 PgUp = pygame_gui.elements.UIButton(
-    relative_rect=pygame.Rect((500, 350), (100, 50)),
+    relative_rect=pygame.Rect((550, 350), (50, 50)),
     text='+',
     manager=manager
 )
 PgDown = pygame_gui.elements.UIButton(
-    relative_rect=pygame.Rect((400, 400), (100, 50)),
+    relative_rect=pygame.Rect((550, 400), (50, 50)),
     text='-',
     manager=manager
 )
-
 running = True
 clock = pygame.time.Clock()
 while running:
-    map_file = load_map(my_map)
-    screen.blit(pygame.image.load(map_file), (0, 0))
-    pygame.display.flip()
+    time_delta = clock.tick(60) / 1000.0
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -62,10 +61,16 @@ while running:
         if event.type == pygame.USEREVENT:
             if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
                 if event.ui_element == PgUp:
-                    # my_map.z += 1
-                    print('+')
+                    if my_map.spn >= 0.00078125:
+                        my_map.spn /= 2
                 if event.ui_element == PgDown:
-                    # my_map.z -= 1
-                    print('-')
+                    if my_map.spn <= 50:
+                        my_map.spn *= 2
+        manager.process_events(event)
+    map_file = load_map(my_map)
+    screen.blit(pygame.image.load(map_file), (0, 0))
+    manager.update(time_delta)
+    manager.draw_ui(screen)
+    pygame.display.flip()
 
 pygame.quit()
