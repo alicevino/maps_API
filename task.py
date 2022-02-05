e27c2a04d1946a400cbb2105a5b9a83898e6fcae
samira <miss-x-sam@yandex.ru>
2022-02-05 13:06:15 +0300

    задание 1

---
+++ task.py
@@ -0,0 +1,50 @@
+# https://github.com/alicevino/maps_API
+import pygame
+import requests
+import os
+import sys
+
+
+class Params(object):
+    def __init__(self):
+        self.latitude = 55.753630
+        self.longitude = 37.620070
+        self.z = 10  # масштаб
+        self.type = 'map'  # Другие значения "sat", "sat,skl"
+
+    def ll(self):
+        answer = str(self.longitude) + ',' + str(self.latitude)
+        return answer
+
+
+def load_map(map):
+    map_request = "http://static-maps.yandex.ru/1.x/?ll={ll}&z={z}&l={type}".format(ll=map.ll(),
+                                                                                        z=map.z, type=map.type)
+    response = requests.get(map_request)
+    if not response:
+        print('Ошибка:', map_request)
+        print('Http статус:', response.status_code, '(', response.reason, ')')
+        sys.exit(1)
+
+    map_file = 'map.png'
+    try:
+        with open(map_file, 'wb') as f:
+            f.write(response.content)
+    except IOError as exception:
+        print('Ошибка при записи временного файла:', exception)
+        sys.exit(2)
+    return map_file
+
+running = True
+
+pygame.init()
+screen = pygame.display.set_mode((600, 450))
+my_map = Params()
+map_file = load_map(my_map)
+screen.blit(pygame.image.load(map_file), (0, 0))
+pygame.display.flip()
+while pygame.event.wait().type != pygame.QUIT:
+    pass
+pygame.quit()
+
+os.remove(map_file)
